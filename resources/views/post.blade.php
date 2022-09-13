b8d43f6d5da9739c39f18d0e5c6c5ef47c4ea25f
Željko Livaja <zeljaos@gmail.com>
2022-09-13 18:30:16 +0200

    laravel 8 - laracast

---
+++ resources/views/post.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>My blog</title>
+    <link rel="stylesheet" href="app.css">
+</head>
+
+<article>
+
+    <?= $post ?>
+
+</article>
+
+
+<a href="/">Go back</a>
+
+</html>
---
+++ resources/views/posts.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>My blog</title>
+    <link rel="stylesheet" href="app.css">
+</head>
+
+<article>
+    <h1> <a href="/posts/my-first-post">First post</a></h1>
+
+    <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officia dicta,
+        magnam ipsam, pariatur aliquid hic,
+        cupiditate velit officiis voluptate tempora enim cumque facilis esse commodi eveniet
+        repudiandae corporis molestiae tenetur.
+    </p>
+</article>
+
+<article>
+    <h1> <a href="/posts/my-second-post">Second post</a></h1>
+
+    <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officia dicta,
+        magnam ipsam, pariatur aliquid hic,
+        cupiditate velit officiis voluptate tempora enim cumque facilis esse commodi eveniet
+        repudiandae corporis molestiae tenetur.
+    </p>
+</article>
+
+<article>
+    <h1> <a href="/posts/my-third-post">Third post</a></h1>
+
+    <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officia dicta,
+        magnam ipsam, pariatur aliquid hic,
+        cupiditate velit officiis voluptate tempora enim cumque facilis esse commodi eveniet
+        repudiandae corporis molestiae tenetur.
+    </p>
+</article>
+
+</html>
