d14224ec05bbc30990de8d2e46932b42a4d2cfd5
Željko Livaja <zeljaos@gmail.com>
2022-09-21 11:27:36 +0200

    episode 28 Factories

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -23,65 +23,8 @@ class DatabaseSeeder extends Seeder
         Post::truncate();
 
 
-        $user = User::factory()->create();
+        // $user = User::factory()->create();
 
-        $personal = Category::create([
-            'name' => 'Personal',
-            'slug' => 'personal'
-        ]);
-
-        $work = Category::create([
-            'name' => 'Work',
-            'slug' => 'work'
-        ]);
-
-        $family = Category::create([
-            'name' => 'Familly',
-            'slug' => 'familly'
-        ]);
-
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' => $family->id,
-            'title' => 'Family post',
-            'slug' => 'family-post',
-            'excerpt' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-            'body' => '<p>Maecenas blandit lorem metus, nec varius felis pellentesque et. 
-            Duis vestibulum nisi sem. Maecenas pretium massa vel libero volutpat, 
-            sit amet sodales ipsum lobortis.
-            Maecenas efficitur orci ante, ac ultricies diam laoreet vitae. 
-            Maecenas posuere sem vitae semper euismod</p>'
-        ]);
-
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' => $work->id,
-            'title' => 'Work post',
-            'slug' => 'work-post',
-            'excerpt' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-            'body' => '<p>Maecenas blandit lorem metus, nec varius felis pellentesque et. 
-            Duis vestibulum nisi sem. Maecenas pretium massa vel libero volutpat, 
-            sit amet sodales ipsum lobortis.
-            Maecenas efficitur orci ante, ac ultricies diam laoreet vitae. 
-            Maecenas posuere sem vitae semper euismod</p>'
-        ]);
-
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' => $personal->id,
-            'title' => 'Personal post',
-            'slug' => 'personal-post',
-            'excerpt' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-            'body' => '<p>Maecenas blandit lorem metus, nec varius felis pellentesque et. 
-            Duis vestibulum nisi sem. Maecenas pretium massa vel libero volutpat, 
-            sit amet sodales ipsum lobortis.
-            Maecenas efficitur orci ante, ac ultricies diam laoreet vitae. 
-            Maecenas posuere sem vitae semper euismod</p>'
-        ]);
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        Post::factory(5)->create();
     }
 }
