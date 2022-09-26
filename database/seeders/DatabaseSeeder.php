b6e005105d3ffe8f3363c57ace64b4c92aea636e
Željko Livaja <zeljaos@gmail.com>
2022-09-26 13:57:49 +0200

    episodes 35,36 frontend and blade components

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -28,7 +28,7 @@ class DatabaseSeeder extends Seeder
             'name' => 'John Doe',
         ]);
 
-        Post::factory(5)->create([
+        Post::factory(20)->create([
             'user_id' => $user->id,
         ]);
     }
