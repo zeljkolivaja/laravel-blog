a5c33903925bbbe8c013d9984a14156def31e3ee
Željko Livaja <zeljaos@gmail.com>
2022-09-29 12:23:52 +0200

    episode 43 fixing eloquent query bug

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -14,7 +14,7 @@
                         <img src="/images/lary-avatar.svg" alt="Lary avatar">
                         <div class="ml-3 text-left">
                             <h5 class="font-bold">
-                                <a href="/?author={{$post->author->username}}">{{$post->author->name}}</a>
+                            <a href="/?author={{$post->author->username}}">{{$post->author->name}}</a>
                             </h5>
                         </div>
                     </div>
